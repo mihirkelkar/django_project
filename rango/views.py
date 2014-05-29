4e67d2df324810ea70e600514b5a0eb75356fb55
mihirkelkar <kelkarmhr@gmail.com>
2014-05-29 15:55:33 -0700

    Adding day 2 content

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,12 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Hello World from the Amazing Bistro !. Learn More about us <a href='/rango/about'>here</a>")
+	context = RequestContext(request)
+	context_dict = {'boldmessage' : "I am a bold font from the context"}
+	return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
 	return HttpResponse("Here is the about Page for the Bistro. <a href = '/rango/'>Go Back</a>")
